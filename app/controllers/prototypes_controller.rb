d5cfa4befe7377ae7c0386f1490a044e45ce2e5d
kazu-oku1 <hawai.waikiki.oahu7@gmail.com>
2022-05-12 19:27:00 +0900

    コード修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  # before_action :set_prototype, except: [:index, :new, :create]
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
@@ -52,13 +52,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
-  # def set_prototype
-    # @prototype = Prototype.find(params[:id])
-  # end
+  def move_to_index
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
 end
